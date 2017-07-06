b357720bdce45f070a30d48fbd56cbd5a98216a7
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 15:07:34 +0000

    fixing errors

--- Devcamp/db/seeds.rb
+++ Devcamp/db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
       title: "My Blog Post {#blog}",
@@ -39,14 +47,6 @@ end
 end
 
 puts "9 portfolio items created"
-
-3.times do |topic|
-  Topic.create!(
-    title: "Topic #{topic}"
-  )
-end
- 
-puts "3 Topics created"
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
--- RoutingApp/config/routes.rb
+++ RoutingApp/config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
   get 'about', to: 'pages#about'
 
-  get 'leadgen/advertising/landingpage/lead', to: 'pages#contact'
+  get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead' 
+  
+  Rails.application.config.root = "../RoutingApp"
 
   resources :blogs
+  resources :posts
+  get 'posts/*missing' , to: 'posts#missing'
+  
+  get 'query/:else/:another_one', to: 'pages#something'
+  get 'query/:else', to: 'pages#something'
+  
+  root to: 'pages#home'
+  
 end
